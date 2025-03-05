2395b99025bc407ff01ac29c7e3c71b1f6712f8a
DevEzro <92102267+DevEzro@users.noreply.github.com>
2025-03-05 11:30:32 +0100

    Update docker_ubuntu_server.sh

--- docker_ubuntu_server.sh
+++ docker_ubuntu_server.sh
@@ -18,4 +18,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 # docker compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
